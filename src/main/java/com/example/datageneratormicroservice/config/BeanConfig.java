50f32ab76ae1af99005e4aa457375d193e630d31
davydov <davydovvip@gmail.com>
2024-03-29 14:24:50 +0500

    Initial commit

---
+++ src/main/java/com/example/datageneratormicroservice/config/BeanConfig.java
@@ -0,0 +1,20 @@
+package com.example.datageneratormicroservice.config;
+
+import com.jcabi.xml.XML;
+import com.jcabi.xml.XMLDocument;
+import lombok.SneakyThrows;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfig {
+
+    @SneakyThrows
+    @Bean
+    public XML producerXml() {
+        return new XMLDocument(
+                getClass().getResourceAsStream("/kafka/producer.xml").readAllBytes()
+        );
+    }
+
+}
